b20930423e95150fddc767d389f4adfbf0fe9b7a
Pratik Anand <pratikanand0512@gmail.com>
2024-12-13 00:45:27 +0530

    updated numericHalfPyramid

--- Patterns/numericHalfPyramid.cpp
+++ Patterns/numericHalfPyramid.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void numericHalfPyramid(int n )
 {
-    for(int row = 1; row < n; row++)
+    for(int row = 1; row <= n; row++)
     {
         for(int col=1; col<=row; col++)
         {
