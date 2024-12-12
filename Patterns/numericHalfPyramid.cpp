7211813345f48b77d04ca64b3a04f75f0cd4bfa5
Pratik Anand <pratikanand0512@gmail.com>
2024-12-13 00:41:53 +0530

    numericHalfPyramid

---
+++ Patterns/numericHalfPyramid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void numericHalfPyramid(int n )
+{
+    for(int row = 1; row < n; row++)
+    {
+        for(int col=1; col<=row; col++)
+        {
+            cout<<col;
+        }
+        cout<<endl;
+    }
+}
+
+
+int main()
+{
+    numericHalfPyramid(6);
+    return 0;
+}
