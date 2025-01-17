062caf21a1c8f9711ede6af87026f8ac1a93f4f5
Pratik Anand <pratikanand0512@gmail.com>
2025-01-18 03:08:54 +0530

    full pyramid

--- Patterns/fullPyramid.cpp
+++ Patterns/fullPyramid.cpp
@@ -21,8 +21,34 @@ void fullPyramid(int n)
     }
 }
 
+void fullPyramidEfficientMethod(int n)
+{
+    for(int i =0; i<n; i++)
+    {
+                    int k =0;
+        for(int j=0; j<((2*n)-1); j++)
+        {
+
+            if(j<n-i-1)
+            {
+                cout<<" ";
+            }
+            else if(k<((2*i)+1))
+            {
+                cout<<"*";
+                k++;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
-    fullPyramid(6);
+    // fullPyramid(4);
+    fullPyramidEfficientMethod(6);
     return 0;
 }
